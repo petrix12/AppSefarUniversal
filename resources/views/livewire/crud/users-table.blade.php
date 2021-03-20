d93f00e2602c044f2346badd8f81eefc3025c1ce
Pedro Jesús Bazó Canelón <bazo.pedro@gmail.com>
2021-03-19 20:39:07 -0400

    Ultimos cambios IV al 19-03-2021

--- resources/views/livewire/crud/users-table.blade.php
+++ resources/views/livewire/crud/users-table.blade.php
@@ -7,7 +7,7 @@
                 <div class="bg-gray-50">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:py-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
                         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                            <span class="ctvSefar block text-indigo-600">{{ __('Users II') }}</span>
+                            <span class="ctvSefar block text-indigo-600">{{ __('Users') }}</span>
                         </h2>
                         @can('crud.users.create')
                         <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
