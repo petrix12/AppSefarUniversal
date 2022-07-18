8cabdc85628fa6896fdf3c095f362e9c17d6ade1
Pedro Bazó <bazo.pedro@gmail.com>
2022-07-18 14:22:35 -0400

    Arreglo favicon 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}">
 
         {{-- Inicio - Personalización URL --}}
         <meta property="fb:app_id" content="APPID">
