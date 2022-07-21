1bd90f8022488cb3e4f6ce3bc2e387a3ce1e0a17
Pedro Bazó <bazo.pedro@gmail.com>
2022-07-20 21:01:32 -0400

    Prueba 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('/js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -32,6 +32,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('/js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
