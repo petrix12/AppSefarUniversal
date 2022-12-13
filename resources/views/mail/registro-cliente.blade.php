ad87e7f404f275b72c50200b606a58dc6438c8df
Pedro Bazó <bazo.pedro@gmail.com>
2022-12-13 10:34:59 -0400

    Nuevo cuerpo correo cliente 2

--- resources/views/mail/registro-cliente.blade.php
+++ resources/views/mail/registro-cliente.blade.php
@@ -69,10 +69,10 @@
                                             En caso de requerir alguna información adicional relacionada con este asunto
                                             o cualquier otro relacionado con el proceso, por favor no dude en comunicarse con nosotros por el
                                             correo electrónico <strong><a href="mailto:info@sefaruniversal.com">info@sefaruniversal.com</a></strong> y/o por los siguientes números de teléfono de atención al cliente:
-                                            USA: <a href="tel:+16032621727">+1 603 2621727</a>
-                                            España: <a href="tel:+34911980993">+34 911 980993</a>
-                                            Venezuela: <a href="tel:+582127201170">+58 212 7201170</a>
-                                            Colombia: <a href="tel:+570353195843">+57 035 3195843</a>
+                                            <p>USA: <a href="tel:+16032621727">+1 603 2621727</a></p>
+                                            <p>España: <a href="tel:+34911980993">+34 911 980993</a></p>
+                                            <p>Venezuela: <a href="tel:+582127201170">+58 212 7201170</a></p>
+                                            <p>Colombia: <a href="tel:+570353195843">+57 035 3195843</a></p>
                                         </p>
                                         <p style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
                                             Atención de lunes a sábado las 24hrs.
