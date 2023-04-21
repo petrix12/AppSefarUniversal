da5784e0b6b693e3c10f12d8576c3aa50cb23ba7
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2023-04-21 13:28:29 -0400

    Update gracias.blade.php

--- resources/views/clientes/gracias.blade.php
+++ resources/views/clientes/gracias.blade.php
@@ -9,25 +9,47 @@
 
 @section('content')
 
-    @csrf
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script type="text/javascript">
-        Swal.fire({
-            icon: 'success',
-            title: '¡Pago procesado correctamente!',
-            html: '<p>En Sefar Universal estamos muy complacido por tenerlo entre nuestros clientes.</p><p>Para continuar con el proceso es muy importante que nos suministre en la medida de los posible toda la información de sus ancestros.</p><p>Por favor presione el botón de <strong>Continuar</strong> para que sea redirigido a nuestra plataforma de carga genealógica.</p><small><p><strong>Nota: </strong>tenga en cuenta que mientras más información genealógica aporte, más fácil y rápido será su proceso de estudio.</p></small></span>',
-                showDenyButton: false,
-                confirmButtonText: 'Continuar',
-                denyButtonText: 'Volver a la Página Principal',
-        }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
-            if (result.isDenied) {
-                window.location.replace("https://www.sefaruniversal.com");
-            } else {
-            	window.location.replace("/getinfo");
-            }
-        });
-    </script>
+    @if (auth()->user()->servicio == 'Recurso de Alzada')
+        @csrf
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="text/javascript">
+            Swal.fire({
+                icon: 'success',
+                title: '¡Pago procesado correctamente!',
+                html: '<p>En Sefar Universal estamos muy complacido por tenerlo entre nuestros clientes.</p><p>En las proximas horas, estaremos comunicándonos con usted para atender su solicitud del <b>Recurso de Alzada</b>.</p><p>Por favor presione el botón de <strong>Continuar</strong> para que sea redirigido a nuestra plataforma de carga genealógica.</p><small><p><strong>Nota: </strong>Si ya usted era cliente antiguo de Sefar Universal, toda su información del genealógica estará disponible para a continuación.</p></small></span>',
+                    showDenyButton: false,
+                    confirmButtonText: 'Continuar',
+                    denyButtonText: 'Volver a la Página Principal',
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isDenied) {
+                    window.location.replace("https://www.sefaruniversal.com");
+                } else {
+                	window.location.replace("/tree");
+                }
+            });
+        </script>
+    @else
+        @csrf
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="text/javascript">
+            Swal.fire({
+                icon: 'success',
+                title: '¡Pago procesado correctamente!',
+                html: '<p>En Sefar Universal estamos muy complacido por tenerlo entre nuestros clientes.</p><p>Para continuar con el proceso es muy importante que nos suministre en la medida de los posible toda la información de sus ancestros.</p><p>Por favor presione el botón de <strong>Continuar</strong> para que sea redirigido a nuestra plataforma de carga genealógica.</p><small><p><strong>Nota: </strong>tenga en cuenta que mientras más información genealógica aporte, más fácil y rápido será su proceso de estudio.</p></small></span>',
+                    showDenyButton: false,
+                    confirmButtonText: 'Continuar',
+                    denyButtonText: 'Volver a la Página Principal',
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isDenied) {
+                    window.location.replace("https://www.sefaruniversal.com");
+                } else {
+                    window.location.replace("/getinfo");
+                }
+            });
+        </script>
+    @endif
 
 @stop
 
