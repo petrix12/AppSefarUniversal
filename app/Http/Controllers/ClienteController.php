5084eb059d21cf8f3fd9ecb506ae563bbff35e4c
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2023-05-21 21:04:43 -0400

    Update ClienteController.php

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -784,6 +784,7 @@ class ClienteController extends Controller
     }
 
     public function checkRegAlzada(Request $request) {
+        dd($request->all())
         $mailpass = json_decode(json_encode(DB::table('users')->where('email', $request->email)->where('passport', $request->numero_de_pasaporte)->get()),true);
         $mail = json_decode(json_encode(DB::table('users')->where('email', $request->email)->get()),true);
 
