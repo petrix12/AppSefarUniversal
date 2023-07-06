9284a8b1aad891d6712ef444a1a23bf40cbb3abc
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2023-07-06 03:42:10 -0400

    Update users-table.blade.php

--- resources/views/livewire/crud/users-table.blade.php
+++ resources/views/livewire/crud/users-table.blade.php
@@ -26,7 +26,7 @@
     </div>
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8" style="max-width: 100%;">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg" >
                     <div class="flex bg-white px-4 py-3 sm:px-6">
                         <input
