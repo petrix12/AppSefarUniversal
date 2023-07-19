6ca7e8f1973247403e90f9c930a0a2da1ad424fe
alejandrosfr1223 <95920858+alejandrosfr1223@users.noreply.github.com>
2023-07-18 21:09:22 -0400

    Update getinfo.blade.php

--- resources/views/clientes/getinfo.blade.php
+++ resources/views/clientes/getinfo.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
 
-    <div style="position: fixed; top: 0; left: 0; background-color:rgba(0, 0, 0, 0.5); z-index: 6000;" id="ajaxload"></div>
+    <div style="position: fixed; top: 0; left: 0; background-color:rgba(0, 0, 0, 0.5); z-index: 6000; width: 100%; height: 100%;" id="ajaxload"></div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
