3057d0147ec2bafd0877529db98a6fe1db03b9db
adiazcommutatio <95920858+alejandrosfr1223@users.noreply.github.com>
2025-01-22 11:06:12 -0400

    APP SEFAR COS

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>sefar</b>',
+    'logo' => '<b>App Sefar - COS</b>',
     'logo_img' => 'vendor/adminlte/dist/img/LogoSefar.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
