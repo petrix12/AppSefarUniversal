bda78d7baaa64ecb58b32e3813883c0f3fcefaaf
adiazcommutatio <95920858+alejandrosfr1223@users.noreply.github.com>
2025-03-15 19:13:38 -0400

    esto del contrato funcionó mejor de lo que yo esperaba...

--- resources/views/clientes/contrato.blade.php
+++ resources/views/clientes/contrato.blade.php
@@ -11,33 +11,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.js" integrity="sha512-tWHlutFnuG0C6nQRlpvrEhE4QpkG1nn2MOUMWmUeRePl4e3Aki0VB6W1v3oLjFtd0hVOtRQ9PHpSfN6u6/QXkQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script type="text/javascript" src="https://form.jotform.com/jsform/231384136753659"></script>
-
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-          var iframe = document.getElementById('231384136753659');
-
-          iframe.addEventListener('load', function() {
-
-              var inputText = iframe.contentWindow.document.getElementById('input_67');
-              var inputText2 = iframe.contentWindow.document.getElementById('input_68');
-              var inputText3 = iframe.contentWindow.document.getElementById('input_329');
-              var inputText4 = iframe.contentWindow.document.getElementById('input_330');
-
-              console.log(inputText); // Para depuración
-
-              inputText.value = '{{auth()->user()->nombres}}';
-              inputText.dispatchEvent(new Event('change')); // Activar evento change
-              inputText2.value = '{{auth()->user()->apellidos}}';
-              inputText2.dispatchEvent(new Event('change'));
-              inputText3.value = '{{auth()->user()->passport}}';
-              inputText3.dispatchEvent(new Event('change'));
-              inputText4.value = '{{auth()->user()->servicio}}';
-              inputText4.dispatchEvent(new Event('change'));
-              
-          });
-        });
-    </script>
+    <iframe style="border: none;" width="100%" height="500px" src="https://www.jotform.com/SfarVzla/isp---firma-de-contrato?nombres={{auth()->user()->nombres}}&nac_solicitada={{auth()->user()->servicio}}&apellidos={{auth()->user()->apellidos}}&nro_pasaporte={{auth()->user()->passport}}&nroDe={{auth()->user()->email}}"></iframe>
 @stop
 
 @section('css')
