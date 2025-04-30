7ba23e6e2738fc5bf8902c419e08fb9ebe8dacfa
adiazcommutatio <95920858+alejandrosfr1223@users.noreply.github.com>
2025-04-30 13:46:29 -0400

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,15 +2,9 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="https://app.sefaruniversal.com/img/logo.png" class="logo" alt="Laravel Logo" style="max-width:100px;
-                background: #093143 !important;
-                border-radius:100px;
-                margin-bottom: 15px;" height="60">
+                <img src="https://app.sefaruniversal.com/img/logo.png" class="logo" alt="Laravel Logo" height="60">
             @else
-                <img src="https://app.sefaruniversal.com/img/logo.png" class="logo" alt="Logo Sefar" style="max-width:100px;
-                background: #093143 !important;
-                border-radius:100px;
-                margin-bottom: 15px;" height="60">
+                <img src="https://app.sefaruniversal.com/img/logo.png" class="logo" alt="Logo Sefar" height="60">
                 <hr>
                 {{ $slot }}
             @endif
