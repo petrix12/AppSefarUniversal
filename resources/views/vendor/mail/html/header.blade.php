0f47633d8db3880e623e77acc8dd4c5d4a7198ad
adiazcommutatio <95920858+alejandrosfr1223@users.noreply.github.com>
2025-04-30 14:08:05 -0400

    logosefar

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="https://app.sefaruniversal.com/img/logo.png" class="logo" alt="Laravel Logo" height="60">
+                <img src="https://app.sefaruniversal.com/img/logo2.png" class="logo" alt="Laravel Logo" height="60">
             @else
-                <img src="https://app.sefaruniversal.com/img/logo.png" class="logo" alt="Logo Sefar" height="60">
+                <img src="https://app.sefaruniversal.com/img/logo2.png" class="logo" alt="Logo Sefar" height="60">
                 <hr>
                 {{ $slot }}
             @endif
