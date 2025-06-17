af398ed2ab6d7baae8ce976010d236ad5476f545
adiazcommutatio <95920858+alejandrosfr1223@users.noreply.github.com>
2025-06-17 11:35:21 -0400

    Update edit.blade.php

--- resources/views/crud/users/edit.blade.php
+++ resources/views/crud/users/edit.blade.php
@@ -344,11 +344,9 @@
                 @if (!$flag)
                     <div class="alert alert-info text-center my-5 p-5" role="alert" style="max-width: 700px; margin: 0 auto; background-color: rgba(0,0,0,0.05); border: 1px solid #b6e0fe; border-radius: 15px;">
                         <center>
-                            <div class="text-center mb-3">
-                                <img class="img-fluid" style="max-width:100px;"
-                                     src="/img/logonormal.png"
-                                     alt="Logo Sefar">
-                            </div>
+                            <img class="img-fluid" style="max-width:100px;"
+                                    src="/img/logonormal.png"
+                                    alt="Logo Sefar">
                             <h4 class="alert-heading"><b>¡Muy pronto disponible!</b></h4>
                             <p class="mt-3">Estamos trabajando para que puedas visualizar el estatus de tu proceso directamente desde esta plataforma.</p>
                             <hr>
