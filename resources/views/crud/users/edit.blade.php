e16e201a5f69acc45c79f2d1f8d65d28f126b8ba
adiazcommutatio <95920858+alejandrosfr1223@users.noreply.github.com>
2025-06-17 11:29:40 -0400

    Update edit.blade.php

--- resources/views/crud/users/edit.blade.php
+++ resources/views/crud/users/edit.blade.php
@@ -344,7 +344,12 @@
                 @if (!$flag)
                     <div class="alert alert-info text-center my-5 p-5" role="alert" style="max-width: 700px; margin: 0 auto; background-color: #e8f4fd; border: 1px solid #b6e0fe; border-radius: 15px;">
                         <center>
-                            <img src="https://cdn-icons-png.flaticon.com/512/4712/4712100.png" alt="En desarrollo" width="100" class="mb-4">
+                            <div class="text-center mb-3">
+                                <img class="img-fluid" style="max-width:100px;"
+                                     src="/vendor/adminlte/dist/img/LogoSefar.png"
+                                     alt="Logo Sefar">
+                                <h4 style="font-weight: bold;">Información de Pago</h4>
+                            </div>
                             <h4 class="alert-heading"><b>¡Muy pronto disponible!</b></h4>
                             <p class="mt-3">Estamos trabajando para que puedas visualizar el estatus de tu proceso directamente desde esta plataforma.</p>
                             <hr>
