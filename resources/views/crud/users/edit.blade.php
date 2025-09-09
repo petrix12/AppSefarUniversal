5415a7f3ff17099472d666e0b12e2aa66d9d3a52
test <95920858+alejandrosfr1223@users.noreply.github.com>
2025-09-09 10:58:50 -0400

    Update edit.blade.php

--- resources/views/crud/users/edit.blade.php
+++ resources/views/crud/users/edit.blade.php
@@ -29,7 +29,7 @@
                     <div class="bg-gray-50">
                         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:py-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
                             <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                                <span class="ctvSefar block text-indigo-600">$user->name</span>
+                                <span class="ctvSefar block text-indigo-600">{{$user->name}}</span>
                             </h2>
                             <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                                 <div class="inline-flex rounded-md shadow">
