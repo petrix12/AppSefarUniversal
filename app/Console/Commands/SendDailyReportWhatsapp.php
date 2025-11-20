eac9dad7a9b44defecf2cd0da29c306cdf87a75a
test <95920858+alejandrosfr1223@users.noreply.github.com>
2025-11-20 08:34:08 -0400

    Update SendDailyReportWhatsapp.php

--- app/Console/Commands/SendDailyReportWhatsapp.php
+++ app/Console/Commands/SendDailyReportWhatsapp.php
@@ -483,9 +483,7 @@ class SendDailyReportWhatsapp extends Command
             foreach ($numbers as $number) {
                 $this->info('Enviando a: ' . $number->phone_number);
 
-                $this->info('Usando URL: ' . $url);
-
-                $response = Http::timeout(120)->post($url, [
+                $response = Http::timeout(120)->post($url->url . '/send-file', [
                     'number' => $number->phone_number,
                     'message' => "📊 *Reporte Diario*\n\nFecha: {$peticion['dia']}/{$peticion['mes']}/{$peticion['año']}",
                     'fileData' => $pdfBase64,
